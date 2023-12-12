015ef4b22ba608227a29cb1700996f3d9f1e23a8
Murat-Karakaya <muratkrky09@gmail.com>
2023-12-12 21:49:10 +0300

    kanser teşhis

--- src/components/pageContents/hastaContents/KanserTeshis.jsx
+++ src/components/pageContents/hastaContents/KanserTeshis.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react"
 
 export default ()=>{
-    const [preview, setPreview] = useState();
+    const [preview, setPreview] = useState()
 
-    const handleFileChange = (event) => {
-        const file = event.target.files[0];
+    const handleFileChange = ({files}) => {
+        const file = files[0];
         const reader = new FileReader();
 
         reader.onloadend = () => {
@@ -23,24 +23,31 @@ export default ()=>{
                 <select className="cancer-select" /* name="cancer-select" */>
                     <option hidden defaultValue="">Kanser Türü Seçiniz</option>
                     <option value="akciğer kanseri">Tür: Akciğer Kanseri</option>
-                    <option value="meme kanseri">Tür: Meme Kanseri</option>
+                    <option value="meme kanseri">Tür: Göğüs Kanseri</option>
                     <option value="deri kanseri">Tür: Deri Kanseri</option>
-                    <option value="akciğer kanseri">Tür: Akciğer Kanseri</option>
+                    <option value="akciğer kanseri">Tür: Lenf Nodu Kanseri</option>
                 </select>
                 <div className="info">Görüntünün JPEG formantında olması gerekmektedir.</div>
                 {/* <input type="file" name="image-file-input" id="image-file-input" accept="image/jpeg"/> */}
                 <div>
-                    <label htmlFor="image-file-input" className="file-upload">Pick an image</label>
+                    <label
+                     tabIndex="0"
+                     role="button" 
+                     htmlFor="image-file-input" 
+                     className="file-upload">
+                        Görüntü Seç
+                    </label>
+                    
                     <input
                      type="file" 
                      name="image-file-input" 
                      id="image-file-input" 
                      accept="image/jpeg" 
                      style={{display: "none"}}
-                     onChange={handleFileChange}/>
-                    
+                     onChange={event => {handleFileChange(event.target)}}/>
                 </div>
-                <img src={preview} alt="Image preview..." />
+                <button className="form-submit">Devam</button>
+                {preview && <img src={preview} height={100} width={"auto"} alt="yüklenen görüntü" /> }
             </fieldset>
         </>
     )
